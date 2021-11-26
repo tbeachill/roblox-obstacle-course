81b8c0ddfc2ec656325d44db40bd3a12f0ecb24d
tbeachill <82418020+tbeachill@users.noreply.github.com>
2021-11-26 17:32:08 +0000

    added group name tag

--- src/server/Chat.lua
+++ src/server/Chat.lua
@@ -8,6 +8,7 @@ local tags = {
 	[194240] = {TagText = "OWNER", TagColor = Color3.fromRGB(255, 59, 59)},
     [274390034] = {TagText = "OWNER", TagColor = Color3.fromRGB(255, 59, 59)},
     ["VIP"] = {TagText = "VIP", TagColor = Color3.fromRGB(241, 140, 72)},
+	["DS"] = {TagText = "DS", TagColor = Color3.fromRGB(230, 241, 72)},
 }
 
 chatService.SpeakerAdded:Connect(function(playerName)
@@ -18,6 +19,9 @@ chatService.SpeakerAdded:Connect(function(playerName)
 		speaker:SetExtraData("Tags", {tags[player.UserId]})
 	elseif player.HiddenData.VIP.Value == true then 
         speaker:SetExtraData("Tags", {tags["VIP"]})
+
+	elseif player:IsInGroup(13039075) then
+		speaker:SetExtraData("Tags", {tags["DS"]})
     end
 end)
 
